7e64471619d263aeca2c5bca042aea161d7256ba
Lukasz Brzozko <lukasz9099@gmail.com>
2020-06-23 15:28:00 +0200

    Use custom format date in GlobalVars

--- src/components/GlobalVars.js
+++ src/components/GlobalVars.js
@@ -1,8 +1,14 @@
 export const arrowType = "fas fa-angle-down";
-const date = new Date();
-const year = date.getFullYear();
 
-const today = date.toISOString().slice(0, 10);
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const today = `${year}-${month < 10 ? "0" + month : month}-${
+  day < 10 ? "0" + day : day
+}`;
+
 const dateWithoutYear = today.slice(4);
 const minYearValue = `${year - 1}${dateWithoutYear}`;
 const maxYearValue = `${year + 1}${dateWithoutYear}`;
